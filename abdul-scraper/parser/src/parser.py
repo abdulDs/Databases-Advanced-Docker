c36fbfa663b978bf8d1a1f19bc8473fb4d0f4361
abdulDs <r0690133@student.thomasmore.be>
2020-12-23 18:01:15 +0100

    version 3

--- abdul-scraper/parser/src/parser.py
+++ abdul-scraper/parser/src/parser.py
@@ -3,22 +3,20 @@ import numpy as np
 import pandas as pd
 from pymongo import MongoClient
 import redis
-import pyarrow as pa
-import redis
+import json
 
-context = pa.default_serialization_context()
+
 redis = redis.Redis(host='localhost', port=6379)
 client = MongoClient('localhost', port=27017)
 # insert the top value in our mongodb
 def storeToMongodb():
-    observations =  redis.get('key')
-    topObservation = context.deserialize(observations).sort_values(by=['Amount (USD)'], ascending=False).head(1).iloc[0].to_dict()
+    observations = pd.read_json(redis.get('key'))
+    topObservation = observations.sort_values(by=['Amount (USD)'], ascending=False).head(1).iloc[0]
     db = client.largest_btc
     notebook = db.notebook
     print(topObservation)
-    notebook.insert_one(topObservation)
+    notebook.insert_one(topObservation.to_dict())
 
 while True:
     storeToMongodb()
-
     time.sleep(63)
--- abdul-scraper/scraper/src/scraper.py
+++ abdul-scraper/scraper/src/scraper.py
@@ -1,16 +1,15 @@
 import urllib.request
 from bs4 import BeautifulSoup
-import requests
 import time
 import numpy as np
 import pandas as pd
 from pymongo import MongoClient
-import urllib.parse
 import redis
-import pyarrow as pa
 import sys
+import json
 
 redis = redis.Redis(host='localhost', port=6379)
+
 def scrapes_BTC_Data():
     r = urllib.request.urlopen('https://www.blockchain.com/btc/unconfirmed-transactions')
     soup = BeautifulSoup(r, 'html.parser')
@@ -29,15 +28,15 @@ def scrapes_BTC_Data():
     stripedList = [s.strip('$')for s in USDamountList]
     amount = [float(s.replace(',','')) for s in stripedList]
 
-    df = pd.DataFrame(list (zip(hashList,timeList,BTCamountList,amount)),
+    return pd.DataFrame(list (zip(hashList,timeList,BTCamountList,amount)),
                         columns = ["Hash", "Time", "Amount (BTC)", "Amount (USD)"])
 
 
-    ## caching the data into redis meomory and delet it after 60 sec
-    context = pa.default_serialization_context()
-    redis.set('key',context.serialize(df).to_buffer().to_pybytes())
-    redis.expire('key',60)
 
+    ## caching the data into redis meomory and delet it after 60 se
 while True:
-    scrapes_BTC_Data()
-    time.sleep(60)
+    print(scrapes_BTC_Data())
+    context = pa.default_serialization_context()
+    redis.set('key',scrapes_BTC_Data().to_json())
+    redis.expire('key',60)
+    time.sleep(63)
