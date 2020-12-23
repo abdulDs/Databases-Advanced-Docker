6805947bbea3d2f6d1ed0bcf077de9f94db17f62
abdulDs <r0690133@student.thomasmore.be>
2020-12-23 16:59:44 +0100

    version 2.2

--- abdul-scraper/parser/src/parser.py
+++ abdul-scraper/parser/src/parser.py
@@ -4,22 +4,21 @@ import pandas as pd
 from pymongo import MongoClient
 import redis
 import pyarrow as pa
+import redis
 
-
-def get_insert():
-    redis = redis.Redis(host='localhost', port=6379)
-    context = pa.default_serialization_context()
-    # git the cached data and sort the highest value
-    topValue = context.deserialize(redis.get('key')).sort_values(
-        by=['Amount (USD)'], ascending=False).head(1).iloc[0].to_dict()
-
-    client = MongoClient('mongod', port=27017)
-    # insert the top value in our mongodb
+context = pa.default_serialization_context()
+redis = redis.Redis(host='localhost', port=6379)
+client = MongoClient('localhost', port=27017)
+# insert the top value in our mongodb
+def storeToMongodb():
+    observations =  redis.get('key')
+    topObservation = context.deserialize(observations).sort_values(by=['Amount (USD)'], ascending=False).head(1).iloc[0].to_dict()
     db = client.largest_btc
     notebook = db.notebook
-    notebook.insert_one(topValue)
+    print(topObservation)
+    notebook.insert_one(topObservation)
 
+while True:
+    storeToMongodb()
 
-while (true):
-    get_insert()
-    time.sleep(60)
+    time.sleep(63)
--- abdul-scraper/scraper/src/scraper.py
+++ abdul-scraper/scraper/src/scraper.py
@@ -1,46 +1,43 @@
+import urllib.request
 from bs4 import BeautifulSoup
 import requests
 import time
 import numpy as np
 import pandas as pd
+from pymongo import MongoClient
 import urllib.parse
 import redis
 import pyarrow as pa
+import sys
 
+redis = redis.Redis(host='localhost', port=6379)
+def scrapes_BTC_Data():
+    r = urllib.request.urlopen('https://www.blockchain.com/btc/unconfirmed-transactions')
+    soup = BeautifulSoup(r, 'html.parser')
+    hashList=[]
+    timeList=[]
+    BTCamountList=[]
+    USDamountList=[]
 
-def scrape_cach():
-    redis = redis.Redis(host='redis', port=6379)
-    # this program scrapes the data from the website
-
-    r = requests.get('https://www.blockchain.com/btc/unconfirmed-transactions')
-    soup = BeautifulSoup(r.text, 'lxml')
-
-    hashList = []
-    timeList = []
-    BTCamountList = []
-    USDamountList = []
-
-    a = soup.findAll('div', {"class": "sc-1g6z4xm-0 arCxa"})
+    a = soup.findAll('div', {"class":"sc-1g6z4xm-0 hXyplo"})
     for element in a:
-        hashList.append(element.findAll(['span', 'a'])[1].text)
-        timeList.append(element.findAll(['span', 'a', ])[3].text)
-        BTCamountList.append(element.findAll(['span', 'a'])[5].text)
-        USDamountList.append(element.findAll(['span', 'a', ])[7].text)
+        hashList.append(element.findAll(['span','a'])[1].text)
+        timeList.append(element.findAll(['span','a',])[3].text)
+        BTCamountList.append(element.findAll(['span','a'])[5].text)
+        USDamountList.append(element.findAll(['span','a',])[7].text)
 
     stripedList = [s.strip('$')for s in USDamountList]
-    amount = [float(s.replace(',', '')) for s in stripedList]
+    amount = [float(s.replace(',','')) for s in stripedList]
 
-    df = pd.DataFrame(list(zip(hashList, timeList, BTCamountList, amount)),
-                      columns=["Hash", "Time", "Amount (BTC)", "Amount (USD)"])
+    df = pd.DataFrame(list (zip(hashList,timeList,BTCamountList,amount)),
+                        columns = ["Hash", "Time", "Amount (BTC)", "Amount (USD)"])
 
-    # cache the data
-    # caching the data into redis meomory and delet it after 60 sec
-    # undrerstand this ????
+
+    ## caching the data into redis meomory and delet it after 60 sec
     context = pa.default_serialization_context()
-    redis.set('key', context.serialize(df).to_buffer().to_pybytes())
-    redis.expire('key', 60)
+    redis.set('key',context.serialize(df).to_buffer().to_pybytes())
+    redis.expire('key',60)
 
-
-while (true):
-    scrape_cach()
+while True:
+    scrapes_BTC_Data()
     time.sleep(60)
